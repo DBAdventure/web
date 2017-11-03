675ea348854b0a1be2ec1b605e1abf45dc37a255
Pierre RAMBAUD <pierre.rambaud86@gmail.com>
2017-11-03 12:26:34 +0100

    Remove unwanted console log

--- middleware/auth.js
+++ middleware/auth.js
@@ -2,7 +2,6 @@ export default async ({store, error}) => {
     await store.dispatch('fetchPlayer');
     await store.dispatch('fetchGameData');
 
-    console.log(store.state.player.connected);
     if (!store.state.player.connected) {
         error({
             message: 'error.not.connected',
