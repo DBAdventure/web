7307858f7772fd3e382ff8b7e6fa87c460954dee
Pierre RAMBAUD <pierre.rambaud86@gmail.com>
2017-11-03 11:05:15 +0100

    Translations

--- pages/register.vue
+++ pages/register.vue
@@ -7,9 +7,9 @@
         <Form ref="registerForm" id="register-form" class="form-horizontal" :rules="registerRules" :model="player">
             <h2>{{ $t('register.personalinfo') }}</h2>
 
-            <Form-item :label="$t('form.name')" :label-width="150" prop="username" required>
+            <Form-item :label="$t('form.pseudo')" :label-width="150" prop="username" required>
                 <Input name="name"
-                       :placeholder="$t('form.name')"
+                       :placeholder="$t('form.pseudo')"
                        v-model="player.name"
                        type="text" />
             </Form-item>
