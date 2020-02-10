db382f298f4c94afdd5a973b0b257a948d4acb87
Titiftw <59482694+Titiftw@users.noreply.github.com>
2020-02-10 20:51:14 +0100

    Added ecosystem file - used in pm2

---
+++ config/ecosystem.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  apps: [
+    {
+      name: 'dev',
+      script: 'npm',
+      args: 'run dev',
+    },
+    {
+      name: 'prod',
+      script: 'npm',
+      args: 'run start',
+    },
+  ],
+};
