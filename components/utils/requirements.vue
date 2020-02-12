2a4605ab648d63bdfb4764eb427654fcc39a2143
Titiftw <59482694+Titiftw@users.noreply.github.com>
2020-02-12 14:45:04 +0100

    Added requirements on spell Margin left requirements

--- components/utils/requirements.vue
+++ components/utils/requirements.vue
@@ -1,11 +1,19 @@
 <template>
-  <ul class="requirements">
-    <li
-      v-for="(value, key) in data"
-      :key="key"
-      v-html="$t(`requirements.details.${key}`, {value})"
-    />
-  </ul>
+  <div
+    v-if="hasData(data)"
+    class="details"
+  >
+
+    <strong>{{ $t('requirements.name') }}</strong>
+
+    <ul class="requirements">
+      <li
+        v-for="(value, key) in data"
+        :key="key"
+        v-html="$t(`requirements.details.${key}`, {value})"
+      />
+    </ul>
+  </div>
 </template>
 
 <script type="text/ecmascript-6">
@@ -14,6 +22,13 @@
       data: {
         type: [Object, Array],
         required: true,
+      },
+    },
+    methods: {
+      hasData(data) {
+        console.log(data);
+
+        return Object.keys(data).length > 0;
       },
     },
   };
